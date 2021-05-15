8ae2c7e80438b13ab733e8903b17d1a89283fb92
Shubham Prakash <chimps.shubham2373@gmail.com>
2021-05-15 16:32:53 +0530

    Create Extended AppBar #2

---
+++ App/todo_app/lib/customAppBar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends PreferredSize {
+  final Widget child;
+  final double height;
+  final Color color;
+  final double bottomRadius;
+
+  CustomAppBar({
+    @required this.child,
+    this.height = kToolbarHeight,
+    this.color = Colors.white,
+    this.bottomRadius = 60,
+  });
+
+  @override
+  Size get preferredSize => Size.fromHeight(height);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: preferredSize.height,
+      // color: Colors.transparent,
+      alignment: Alignment.topCenter,
+      decoration: ShapeDecoration(
+        shape: ContinuousRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(bottomRadius),
+            bottomRight: Radius.circular(bottomRadius),
+          ),
+        ),
+        color: color,
+      ),
+      child: child,
+    );
+  }
+}
--- App/todo_app/lib/main.dart
+++ App/todo_app/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/customAppBar.dart';
+
+import 'models/globals.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,20 +25,59 @@ class Home extends StatelessWidget {
       length: 3,
       child: SafeArea(
         child: Scaffold(
-          appBar: TabBar(
-            unselectedLabelColor: Colors.black,
-            labelColor: Colors.yellow,
-            physics: BouncingScrollPhysics(),
-            indicatorPadding: EdgeInsets.symmetric(horizontal: 12),
-            tabs: [
-              Tab(icon: Icon(Icons.ac_unit_rounded)),
-              Tab(icon: Icon(Icons.ad_units)),
-              Tab(icon: Icon(Icons.backpack_outlined)),
-            ],
+          extendBodyBehindAppBar: true,
+          appBar: CustomAppBar(
+            height: 200,
+            bottomRadius: 80,
+            child: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TabBar(
+                      indicatorWeight: 0.001,
+                      unselectedLabelColor: Colors.black,
+                      labelColor: Colors.yellow,
+                      physics: BouncingScrollPhysics(),           
+                      indicatorColor: Colors.transparent,
+                      tabs: [
+                        Tab(icon: Icon(Icons.ac_unit_rounded)),
+                        Tab(icon: Icon(Icons.ad_units)),
+                        Tab(icon: Icon(Icons.backpack_outlined)),
+                      ],
+                    ),
+                    // Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 40, bottom: 40),
+                      child: Text(
+                        'InTray',
+                        style: intrayTextStyle,
+                      ),
+                    )
+                  ],
+                ),
+                Positioned(
+                  height: 80,
+                  width: 80,
+                  bottom: -40,
+                  left: MediaQuery.of(context).size.width * 0.5 - 40,
+                  child: FloatingActionButton(
+                    onPressed: () {},
+                    backgroundColor: redColor,
+                    child: Icon(
+                      Icons.add,
+                      size: 60,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
           body: TabBarView(
             children: [
-              Container(color: Colors.blue),
+              Container(color: darkGreyColor),
               Container(color: Colors.red),
               Container(color: Colors.yellow),
             ],
